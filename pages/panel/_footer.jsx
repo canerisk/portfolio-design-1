c3ba2931badf79daccda055310338b0b37ed60e8
raqzid <96196481+raqzid@users.noreply.github.com>
2022-07-24 21:39:39 +0300

    Update _footer.jsx

--- pages/panel/_footer.jsx
+++ pages/panel/_footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
 <>
         <div>
             <div className='w-full py-4 lg:py-0 md:py-0 sm:py-0 text-xs mt-32 bg-gray-100 lg:px-[12.5rem] md:px-[7rem] sm:px-[4rem] px-[2rem]  lg:h-9 md:h-9 sm:h-9  text-center lg:flex md:flex sm:flex   items-center justify-between'>
-                <h1>raqzid.com © 2022, All rights reserved.</h1>
+                <h1>raqzid.live © 2022, All rights reserved.</h1>
                 <div>
                     <h1 className='lg:pt-0 md:pt-0 sm:pt-0 pt-1'>Ampac, SyneS, geyGweep ❤️</h1>
                 </div>
